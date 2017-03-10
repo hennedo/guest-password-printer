c0c822510048ae12a901a89a4780f3f8b317326e
henne <henne@drunkenrecords.de>
2017-03-10 13:54:59 +0100

    this should be better

--- index.js
+++ index.js
@@ -4,15 +4,24 @@ var SerialPort = require('serialport'),
     }),
     Printer = require('thermalprinter');
 
+// either set environment variables (recommended) or delete until _here_
+if(!process.env.PASSWORD || !process.env.USERNAME || !process.env.HOST || !process.env.SSID) {
+  console.log("Please set the Environment Variables PASSWORD, USERNAME and HOST to your controller credentials and tell us with SSID the SSID of your wifi");
+  process.exit(1);
+}
+//_here
+// and set the variables manually here
+var config = {
+  username: process.env.USERNAME, // username: "niceuser1",
+  password: process.env.PASSWORD, // password: "secretpassword43",
+  host: process.env.HOST, // host: "www.myunificontroller.gov",
+  ssid: process.env.SSID // ssid: "myssid"
+}
 
 var Unifi = require('unifi-api');
-//var Ruckus = require('./ruckus.js');
+//var Ruckus = require('./ruckus.js'); // ruckus coming later.
 
-var api = new Unifi({
-  password: 'xxxxxx',
-  username: 'henne',
-  host: '10.211.55.4'
-});
+var api = new Unifi(config);
 var printer;
 
 function generateCode() {
@@ -25,12 +34,12 @@ function generateCode() {
   });
 }
 
-
+// feel free to change what it prints ;)
 function printCode(code) {
   printer
       .center()
       .printLine('=====================')
-      .printLine('WLAN: Drunken Records')
+      .printLine('WLAN: ' + config.ssid)
       .printLine('+++++++++++++++++++++')
       .printLine('Code: ' + code)
       .printLine('+++++++++++++++++++++')
@@ -44,11 +53,7 @@ function printCode(code) {
       });
 }
 
-setTimeout(function() {
- //generateCode();
- printCode("test")
-}, 1000);
-
 serialPort.on('open',function() {
     printer = new Printer(serialPort);
+    generateCode();
 });
